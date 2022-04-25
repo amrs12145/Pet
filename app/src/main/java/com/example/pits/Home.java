1711d9c1b7cba45d38e9a60a9553fc5508538ea9
Zeyad <zieadhammad15@gmail.com>
2022-04-25 17:15:06 +0200

    receiver

--- app/src/main/java/com/example/pits/Home.java
+++ app/src/main/java/com/example/pits/Home.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,6 +21,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Home extends AppCompatActivity {
+    private ArrayList<String>mWordList;
+    private RecyclerView myRecyclerView;
+    private HomeReceiver customReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +34,17 @@ public class Home extends AppCompatActivity {
         createCustomToolbar();
 
         //myRecycle View
-        myRecycleView();
+        myRecycleView(myRecyclerView,mWordList);
 
+        //customBroadcastReceiver
+        customHomeReceiver(customReceiver);
     }
-  //**********//myRecycle View
-    private void myRecycleView() {
-        ArrayList<String>mWordList;
-        RecyclerView myRecyclerView;
+
+
+
+    //**********//myRecycle View
+    private void myRecycleView(RecyclerView myRecyclerView ,ArrayList<String>mWordList) {
+
         mWordList=new ArrayList<>();
         mWordList.add("Text1");
         mWordList.add("Text2");
@@ -81,6 +90,25 @@ public class Home extends AppCompatActivity {
         }
     }
     //**************OptionsMenu mytoolbar
+    private void customHomeReceiver(HomeReceiver customReceiver) {
+        customReceiver=new HomeReceiver();
+
+        IntentFilter filter = new IntentFilter(); //Specify type of Actions will be received by customReceiver
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_HEADSET_PLUG);
 
 
+        // Register the receiver using the activity context.
+        this.registerReceiver(customReceiver, filter);
+    }
+    //**********customHomeReceiver
+
+    //onDestroy
+    @Override
+    protected void onDestroy() {
+        //Unregister the receiver
+        this.unregisterReceiver(customReceiver);
+        super.onDestroy();
+    }
 }
