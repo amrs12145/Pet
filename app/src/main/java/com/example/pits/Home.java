ce97097ceb23433c6dd8d001d74a48decac08f7e
amrs12145 <amrs12145@gmail.com>
2022-04-25 15:20:43 +0200

    Creating 3 screens: Welcome, Orientation and Home

---
+++ app/src/main/java/com/example/pits/Home.java
@@ -0,0 +1,67 @@
+package com.example.pits;
+
+//import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //create custom toolbar rather than default Actionbar
+        createCustomToolbar();
+
+
+    }
+
+    //**************OptionsMenu mytoolbar
+    public void createCustomToolbar(){
+
+        // Find the toolbar view and set as ActionBar
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menutoolbar, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.notification:
+                Toast.makeText(this,"notification",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.item1:
+                Toast.makeText(this,"item1",Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+    //**************OptionsMenu mytoolbar
+
+
+}
